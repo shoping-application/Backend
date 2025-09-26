c4587ab0e2b3431466b6e4700feede9b6f82e65e
Biplab Biswas <bbiplab165@gmail.com>
2025-09-26 19:41:44 +0530

    backend

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const userCartRoutes=require("./routes/userCartRoutes")
 
 app.use(
   cors({
-    origin: "https://swiftmartshopping.netlify.app/",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    origin: "https://swiftmartshopping.netlify.app",
     credentials: true,
   })
 );
